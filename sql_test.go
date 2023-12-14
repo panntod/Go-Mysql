df5e5d350f95aef76bbe7e6b8e13a1d1d4bccf33
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-14 07:37:01 +0700

    feat: added function read data from database

--- sql_test.go
+++ sql_test.go
@@ -19,3 +19,31 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Data Successfully Inserted")
 }
+
+func TestExecQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	selectSql := "SELECT * FROM `go-user`"
+	row, err := db.QueryContext(ctx, selectSql)
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var (
+			id     int
+			nama   string
+			umur   int
+			alamat string
+		)
+
+		err = row.Scan(&id, &nama, &umur, &alamat)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("id: %d, nama: %s, umur: %d, alamat: %s \n", id, nama, umur, alamat)
+	}
+}
