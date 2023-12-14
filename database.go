46f56d19cff8a004eb9ceac39b8e723f890e0281
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-14 07:36:06 +0700

    feat: added function connection to database

---
+++ database.go
@@ -0,0 +1,20 @@
+package gomysql
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/cafe")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxIdleTime(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
